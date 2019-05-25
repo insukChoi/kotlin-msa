6831c55b97bf130d3d72517c611cd07475498b57
insuk <cis1725@gmail.com>
2019-05-25 15:26:34 +0900

    Config 서버 셋팅 및 연동

---
+++ ch06/src/main/kotlin/com/microservies/ch06/Ch06Application.kt
@@ -0,0 +1,11 @@
+package com.microservies.ch06
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Ch06Application
+
+fun main(args: Array<String>) {
+    runApplication<Ch06Application>(*args)
+}
---
+++ ch06/src/test/kotlin/com/microservies/ch06/Ch06ApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.microservies.ch06
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class Ch06ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
